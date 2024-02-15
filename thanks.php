d799195f0994ba908718dedcbf6b90eaf722d242
Lambseb <lambranca.s@gmail.com>
2024-02-15 21:30:18 +0100

    :sparkles::sparkles: advanced php form

--- thanks.php
+++ thanks.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php post</title>
+</head>
+<body>
+    
+
 <?php 
     $firstName = $_POST['firstName'];
     $lastName =$_POST['lastName'];
@@ -6,8 +16,11 @@
     $courriel = $_POST['courriel'];
     $message = $_POST['message'];
 ?>
+<article>
 <p>Merci <?= $firstName?>  <?= $lastName?>  de nous avoir contacté à propos de "<?=$choiceObject ?>".</p>
 
 <p>Un de nos conseillers vous contactera soit à l'adresse mail <?= $courriel?> ou par téléphone au <?= $callNumber ?> dans les plus brefs délais pour traiter votre demande:
 <?= $message ?>
-</p> 
+</p> </article>
+</body>
+</html>
